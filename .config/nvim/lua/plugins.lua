466865e3f954d3afc66c010a262bf643cc4fc926
Borgar Barland <borgarbarland@gmail.com>
2024-03-15 00:13:15 +0100

    Added Dart LSP to neovim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -16,8 +16,48 @@ require('packer').startup(function()
     end
   }
 
+  -- LSP config
+  use { 'neovim/nvim-lspconfig' }
+
+  -- Flutter-tools
+  use {
+    'akinsho/flutter-tools.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+  }
+
+  -- nvim-surround
+  use({
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  })
+
   -- Java completions
   use 'mfussenegger/nvim-jdtls'
 
 end)
+
+-- Set up flutter tools
+require("flutter-tools").setup {} -- use defaults
+
+-- Set up Flutter/Dart LSP
+require'lspconfig'.dartls.setup{
+  cmd = {
+    "dart", 
+    "/home/borgar/snap/flutter/common/flutter/bin/cache/dart-sdk/bin/snapshots/analysis_server.dart.snapshot", 
+    "--lsp"
+  },
+  root_dir = function(fname)
+    return require'lspconfig'.util.find_git_ancestor(fname) or require'lspconfig'.util.find_files(".dart_tool/package_config.json")
+  end,
+}
+
+require'nvim-surround'.setup()
 
