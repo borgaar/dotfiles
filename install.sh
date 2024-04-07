f7ea23eb098f0f64105a187818b87c1a84868848
Borgar Barland <borgarbarland@gmail.com>
2024-04-07 10:46:29 +0200

    Extracted settings and made script improvements

--- install.sh
+++ install.sh
@@ -2,70 +2,63 @@
 
 cd $HOME/.dotfiles
 
-# List of paths to link
-paths=(
-	".aliases"
-	".gitconfig"
-	".zshrc"
-	".config/Code/User/keybinds.json"
-	".config/Code/User/settings.json"
-	".config/alacritty/alacritty.toml"
-	".config/alacritty/catppuccin-mocha.toml"
-	".config/figma-linux/settings.json"
-	".config/figma-linux/Themes/Catppuchin_Mocha.json"
-	".config/htop/htoprc"
-	".config/tmux/tmux.conf"
-	".p10k"
-)
+source settings
 
 # Install yay
+if $install_yay ; then
+	echo
+	echo " -- Install yay --> Please enter your password if prompted --"
+	echo
+	sudo pacman -S --needed git base-devel
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	makepkg -si
+	yay -Y --gendb
+	yay -Syu --devel
+	yay -Y --devel --save
+	rm -rf $HOME/.dotfiles/yay
+	echo
+	read -p "EXIT CODE: $? - Press enter to continue"
+	echo
+	echo " -- Running package cleanup and update --> Please enter your password if prompted --"
+	echo
+	yay -Scc && yay -Yc && yay && yay -Yc
+	echo
+	read -p "EXIT CODE: $? - Press enter to continue"
+else
+	echo
+	echo " -- Running package cleanup and update --> Please enter your password if prompted --"
+	echo
+	sudo pacman -Rns $(pacman -Qdtq)
+	sudo pacman -Syu
+	echo
+	read -p "EXIT CODE: $? - Press enter to continue"
+fi
+
+
+
+
+# Install pacman packages
 echo
-echo " -- Install yay --> Please enter your password if prompted --"
+echo " -- Installing pacman packages --> Please enter your password if prompted --"
 echo
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && yay -Y --gendb && yay -Syu --devel && yay -Y --devel --save
+sudo pacman -S $packages
+echo
 read -p "EXIT CODE: $? - Press enter to continue"
 
-# Running update
-echo
-echo " -- Running package cleanup and update --> Please enter your password if prompted --"
-echo
-yay -Scc && yay -Yc && yay && yay -Yc
-read -p "EXIT CODE: $? - Press enter to continue"
 
-# Install zsh
-echo
-echo " -- Installing zsh --> Please enter your password if prompted --"
-echo
-sudo pacman -S zsh
-read -p "EXIT CODE: $? - Press enter to continue"
 
-# Install wl-clipboard
-echo
-echo " -- Installing wl-clipboard --> Please enter your password if prompted --"
-echo
-sudo pacman -S wl-clipboard
-read -p "EXIT CODE: $? - Press enter to continue"
-
-# Install zoxide
-echo
-echo " -- Installing zoxide --> Please enter your password if prompted --"
-echo
-sudo pacman -S zoxide
-read -p "EXIT CODE: $? - Press enter to continue"
 
 # Install powerlevel10k
 echo
 echo " -- Installing powerlevel10k --> Please enter your password if prompted --"
 echo
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.powerlevel10k
+echo
 read -p "EXIT CODE: $? - Press enter to continue"
 
-# Install alacritty
-echo
-echo " -- Installing alacritty --> Please enter your password if prompted --"
-echo
-sudo pacman -S alacritty
-read -p "EXIT CODE: $? - Press enter to continue"
+
+
 
 # Install FiraCode
 echo
@@ -73,27 +66,31 @@ echo " -- Installing FiraCode --> Please enter your password if prompted --"
 echo
 mkdir -p $HOME/.fonts/f/FiraCodeNerdFont_Regular.ttf
 cp FiraCodeNerdFont-Regular.ttf $HOME/.fonts/f/FiraCodeNerdFont_Regular.ttf
+echo
 read -p "EXIT CODE: $? - Press enter to continue"
 
-# Install tree
-echo
-echo " -- Installing tree --> Please enter your password if prompted --"
-echo
-sudo pacman -S tree
-read -p "EXIT CODE: $? - Press enter to continue"
+
+
+
 
 # Change shell
 echo
 echo " -- Changing shell to zsh for $USER --> Please enter your password if prompted and enter '/usr/bin/zsh' afterwards --"
 echo
 chsh $USER
+echo
 read -p "EXIT CODE: $? - Press enter to continue"
+
+
+
+
 
 # Install Oh My Zsh
 echo
-echo " -- Installing Oh My Zsh --"
+echo " -- Installing Oh My Zsh - After the installation type 'exit' and hit enter --"
 echo
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo
 read -p "EXIT CODE: $? - Press enter to continue"
 
 # Install zsh plugins
@@ -102,6 +99,7 @@ echo " -- Installing zsh plugins --> Please enter your password if prompted --"
 echo
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
+echo
 read -p "EXIT CODE: $? - Press enter to continue"
 
 echo
